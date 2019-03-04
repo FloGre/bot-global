e48e3279e37dea9c8f7c120f79521c5a9f4118db
FloGre <48192045+FloGre@users.noreply.github.com>
2019-03-04 22:28:51 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -20,3 +20,30 @@ bot.on('message', message =>{
         console.log("Commande Salut effectué");
     }
 });
+
+bot.on('message', message =>{
+    var msgauthor = message.author.id;
+    if(message.author.bot)return;
+    if(!db.get("xp").find({user: msgauthor}).value()){
+        db.get("xp").push({user: msgauthor, xp: 1}).write();
+    }else
+        var userxpdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
+        console.log(userxpdb);
+        var userxp = Object.values(userxpdb)
+        console.log(userxp)
+        console.log(`Nombre d'xp: ${userxp[1]}`)
+
+        db.get("xp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write();
+
+    if(message.content === prefix + "xp"){
+        var xp = db.get("xp").filter({user: msgauthor}).find('xp').value()
+        var xpfinal = Object.values(xp);
+        var xp_embed = new Discord.RichEmbed()
+            .setTitle(`Stat des XP de ${message.author.username}`)
+            .setColor('#FF0000')
+            .setDescription("Affichage des XP")
+            .addField("XP:", `${xpfinal[1]} xp`)
+            .setFooter("GG")
+        message.channel.send({embed: xp_embed});
+    }
+});
