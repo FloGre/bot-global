cf53b271be86e0da82a70b7a355684785183518a
FloGre <48192045+FloGre@users.noreply.github.com>
2019-03-04 22:23:54 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,13 +1,5 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync')
-const client = new Discord.Client();
-
-const adapter = new FileSync('database.json');
-const db = low(adapter);
-
-db.defaults({ histoires: [], xp: []}).write()
 
 var prefix = ("/")
 
@@ -26,31 +18,5 @@ bot.on('message', message =>{
     if(message.content === "Salut"){
         message.reply("Bien le bonjour. \n Auriez vous l'obligence de me montrer votre culotte ? :angel:");
         console.log("Commande Salut effectué");
-    }
-
-    var msgauthor = message.author.id;
-
-    if(message.author.bot)return;
-    if(!db.get("xp").find({user: msgauthor}).value()){
-        db.get("xp").push({user: msgauthor, xp: 1}).write();
-    }else
-        var userxpdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
-        console.log(userxpdb);
-        var userxp = Object.values(userxpdb)
-        console.log(userxp)
-        console.log(`Nombre d'xp: ${userxp[1]}`)
-
-        db.get("xp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write();
-
-    if(message.content === prefix + "xp"){
-        var xp = db.get("xp").filter({user: msgauthor}).find('xp').value()
-        var xpfinal = Object.values(xp);
-        var xp_embed = new Discord.RichEmbed()
-            .setTitle(`Stat des XP de ${message.author.username}`)
-            .setColor('#FF0000')
-            .setDescription("Affichage des XP")
-            .addField("XP:", `${xpfinal[1]} xp`)
-            .setFooter("GG")
-        message.channel.send({embed: xp_embed});
     }
 });
