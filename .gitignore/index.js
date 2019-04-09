249eb43694a3ccaa593beb72a04c54d51df4011a
FloGre <48192045+FloGre@users.noreply.github.com>
2019-04-09 22:40:49 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,45 +1,23 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-const adapters = new FileSync('database.json');
-const db = low(adapters);
 
-db.defaults({histoires: [], xp: []}).write()
+var bot = new Discord.Client();
+var prefix = ("b!"):
+var randnum = 0;
 
-var prefix = (".")
-
-bot.on('ready', function(){
-    bot.user.setGame("TOUHOU");
+bot.on('ready', ()=>{
+    bot.user.setPresence({ game: { name: `TOUHOU`, type: 0}});
     console.log("Connecté");
 });
 
 bot.login(process.env.TOKEN);
 
-bot.on('message', message =>{
-    var msgauthor = message.author.id;
-    if(message.author.bot)return;
-    if(!db.get("xp").find({user: msgauthor}).value()){
-        db.get("xp").push({user: msgauthor, xp: 1}).write();
-    }else{
-        var userxpdb = db.get("xp").filter({user: msguthor}).find('xp').value();
-        console.log(userxpdb);
-        var userxp = Object.values(userxpdb)
-        console.log(userxpdb);
-        console.log(`Nombre d'xp: ${userxp[1]}`)
-
-        db.get("xp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write)();
-
-        if(message.content === prefix + "xp"){
-            var xp = db.get("xp").filter({user: msgauthor}).find('xp').value()
-            var xpfinal = Object.values(xp);
-            var xp_embed = new Discord.RichEmbed()
-                .setTitle(`Statistiques des XP de ${message.author.username}`)
-                .setColor(`#FF0000`)
-                .setDescription("Affichage des XP")
-                .addField("XP:",`${xpfinal[1]} xp`)
-                .setFooter("Incroyable du cul")
-            message.channel.send({embed: xp_embed})
-        }
+bot.on(`message`, message => {
+    if (message.content === prefix + "help"){
+        var help_embed = new Discord.RichEmbed();
+        .setColor("#FF0000")
+        .addField("Commande du BOT", "  -b!help : Affiche les commandes du BOT")
+        message.channel.sendEmbed(help_embed);
+        //message.channel.send("Voici les coommandes du BOT");
+     console.log("Commande help demandée");
     }
 });
