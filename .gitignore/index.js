c84e1bf2f6bd291fc4c82482780d6f989f21710b
FloGre <48192045+FloGre@users.noreply.github.com>
2019-04-09 11:39:17 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -17,20 +17,6 @@ bot.on('ready', function(){
 bot.login(process.env.TOKEN);
 
 bot.on('message', message =>{
-    if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes: \n /help: afficher les commandes \n /xp: affiche le nombre d'XP \n Salut ou Bonne nuit: le BOT réponds");
-    }
-    if(message.content === "Salut"){
-        message.reply("Bien le bonjour. \n Auriez vous l'obligence de vous la fermer ! MERCI :angel:");
-        console.log("Commande Salut effectué");
-    }
-    if(message.content === "Bonne nuit"){
-        message.reply("Déjà ?! Petite nature !!!");
-        console.log("Commande Bonne nuit effectué");
-    }
-});
-
-bot.on('message', message =>{
     var msgauthor = message.author.id;
     if(message.author.bot)return;
     if(!db.get("xp").find({user: msgauthor}).value()){
